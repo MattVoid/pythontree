91562d8af983e3a68c11f55bc4d4d4b6700f07ab
Matt <am.monteleone.am@gmail.com>
2018-02-21 15:14:40 +0100

    Update tree.py

--- tree.py
+++ tree.py
@@ -2,9 +2,17 @@ import os
 import sys
 from hashlib import md5
 def roots(path = os.popen('cd; pwd').read().split('\n')[0], space = '', start = 0, end = 1):
+	files = path
+	if path == '/':
+		if os.popen('whoami').read().split('\n')[0] == 'root':
+			files = ' /'
+		else:
+			print('for run the function roots from root, run this scritp as a superuser')
+			exit()
+	else:
+		files = path.split('/')[len(os.popen('pwd').read().split('/')) - 1]
 	os.chdir(path)
 	dir = []
-	files = path.split('/')[len(os.popen('pwd').read().split('/')) - 1]
 	path = []
 	pathing2 = os.popen('pwd').read().split('\n')[0]
 	ls = os.popen('ls').read().split('\n')
@@ -35,35 +43,40 @@ def roots(path = os.popen('cd; pwd').read().split('\n')[0], space = '', start = 
 		os.chdir(os.popen('cd "' + dir[i] + '"; ' + 'pwd').read().split('\n')[0])
 		roots(os.popen('pwd').read().split('\n')[0], space + '  │', i, len(dir))
 		os.chdir(pathing2)
+
+
 def clean(clear = os.popen('cd; pwd').read().split('\n')[0], path = [], delete = [], pwd = [], pwd2 = [], file = []):
 	os.chdir(clear)
 	pathing2 = clear
 	ls = os.popen('ls').read().split('\n')
-	for i in range(len(ls)):
-		if os.path.isfile('./' + ls[i]) == True:
-			if len(path) != 0:
-				for x in range(len(path)):
-					if md5(open(ls[i], 'rb').read()).hexdigest() == path[x]:
-						pwd2.append(file[x])
-						pwd.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])
-						delete.append(md5(open(ls[i], 'rb').read()).hexdigest())
-				if len(delete) != 0:
-					if md5(open(ls[i], 'rb').read()).hexdigest() != delete[len(delete) - 1]:
+	if len(ls) - 1 == 0:
+		print(os.popen('pwd').read().split('\n')[0] + "\033[1;31m" + ' is empty' + "\033[0;0m")
+	else:
+		for i in range(len(ls)):
+			if os.path.isfile('./' + ls[i]) == True:
+				if len(path) != 0:
+					for x in range(len(path)):
+						if md5(open(ls[i], 'rb').read()).hexdigest() == path[x]:
+							pwd2.append(file[x])
+							pwd.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])
+							delete.append(md5(open(ls[i], 'rb').read()).hexdigest())
+					if len(delete) != 0:
+						if md5(open(ls[i], 'rb').read()).hexdigest() != delete[len(delete) - 1]:
+							path.append(md5(open(ls[i], 'rb').read()).hexdigest())
+							file.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])
+					elif len(delete) == 0:
 						path.append(md5(open(ls[i], 'rb').read()).hexdigest())
 						file.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])
-				elif len(delete) == 0:
+				else:
 					path.append(md5(open(ls[i], 'rb').read()).hexdigest())
 					file.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])
-			else:
-				path.append(md5(open(ls[i], 'rb').read()).hexdigest())
-				file.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])
-		elif os.path.isdir(ls[i]) == True:
-			os.chdir(os.popen('cd "' + ls[i] + '"; ' + 'pwd').read().split('\n')[0])
-			clean(os.popen('pwd').read().split('\n')[0], path, delete, pwd, pwd2, file)
-			os.chdir(pathing2)
-	less = 1
-	for i in range(len(pwd)):
-		print("\033[;1m" + pwd[i - less] + "\033[1;31m" + ' is same to ' + "\033[;1m" + pwd2[i - less] + "\033[0;0m")
-		pwd.remove(pwd[i - less])
-		pwd2.remove(pwd2[i - less])
-		less = less + 1
+			elif os.path.isdir(ls[i]) == True:
+				os.chdir(os.popen('cd "' + ls[i] + '"; ' + 'pwd').read().split('\n')[0])
+				clean(os.popen('pwd').read().split('\n')[0], path, delete, pwd, pwd2, file)
+				os.chdir(pathing2)
+		less = 1
+		for i in range(len(pwd)):
+			print(pwd[i - less] + "\033[1;31m" + ' is same to ' + "\033[0;0m" + pwd2[i - less])
+			pwd.remove(pwd[i - less])
+			pwd2.remove(pwd2[i - less])
+			less = less + 1
