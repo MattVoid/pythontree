39957597b16f292366a8239010932e13b0674f2a
Matteo Monteleone <am.monteleone.am@gmail.com>
2018-02-25 23:47:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 
 setup(
-   name = 'pythontree',
-   version = '1.3',
-   description = 'facilitate folder management',
-   license = "Apache-2.0",
-   author = 'MattVoid',
-   author_email = 'am.monteleone.am@gmail.com',
-   url = 'https://github.com/MattVoid/pythontree',
-   long_description = read('README'),
-   packages = ['pythontree'],
+	name = 'pythontree',
+	version = '1.3.2',
+	description = 'facilitate folder management',
+	license = "Apache-2.0",
+	author = 'MattVoid',
+	author_email = 'am.monteleone.am@gmail.com',
+	url = 'https://github.com/MattVoid/pythontree',
+	long_description = '',
+	packages = ['pythontree'],
+	classifiers=[
+	"Development Status :: 4 - Beta",
+	],
 )
