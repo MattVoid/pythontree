1802446b6ad60748692e7ee0149a3a93ffb207b0
Matteo Monteleone <am.monteleone.am@gmail.com>
2018-03-01 13:35:22 +0100

    Update __init__.py

--- pythontree/__init__.py
+++ pythontree/__init__.py
@@ -17,8 +17,6 @@ class Tree:
 	x = 0
 	dirs = []
 	files = []
-	# element prova
-	clean = {}
 
 	def __init__(self, path = os.popen('cd; pwd').read().split('\n')[0]):
 		self.path = path
@@ -105,10 +103,7 @@ class Clean(Tree):
 		super().__init__(path)
 		self.path = path
 	# show empty folders and equal files
-	def element(self, show = 'off'):
-		if show != 'on' and show != 'off':
-			print('Arguments not found: the value of this arguments is "on" or "off"')
-			exit()
+	def element(self):
 		os.chdir(self.path)
 		pathing = os.popen('pwd').read().split('\n')[0]
 		ls = os.popen('ls').read().split('\n')
@@ -120,7 +115,7 @@ class Clean(Tree):
 					Tree.dirs.append(ls[i])
 					Tree.x = Tree.x + 1																			# dirs
 					os.chdir(os.popen('cd "' + ls[i] + '"; pwd').read().split('\n')[0])
-					Clean(os.popen('pwd').read().replace('\n', '')).element(show)
+					Clean(os.popen('pwd').read().replace('\n', '')).element()
 					Tree.x = Tree.x - 1
 					os.chdir(pathing)
 				elif os.path.isfile('./' + ls[i]) == True:
@@ -142,23 +137,58 @@ class Clean(Tree):
 						Tree.files.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])				# files
 			less = 1
 			try:
-				if show == 'off':
-					return Tree.empty, Tree.files
-				elif show == 'on':
-					for i in range(len(Tree.empty)):
-						print(Tree.empty[i] + "\033[1;31m" + ' is empty ' + "\033[0;0m")
-					for i in range(len(Tree.pwd)):
-						print(Tree.pwd[i - less] + "\033[1;31m" + ' is same to ' + "\033[0;0m" + Tree.pwd2[i - less])
-						Tree.pwd.remove(Tree.pwd[i - less])
-						Tree.pwd2.remove(Tree.pwd2[i - less])
-						less = less + 1
+				return Tree.empty, Tree.files, Tree.pwd, Tree.pwd2
 			finally:
 				if Tree.x == 0:
 					# clear Clean().element() array
-					Tree.dirs = []
-					Tree.files = []
-					Tree.delete = []
-					Tree.pwd = []
-					Tree.pwd2 = []
-					Tree.clear = []
-					Tree.empty = []
+					Tree.dirs = [] #dirs
+					Tree.files = [] #files
+					Tree.delete = [] #delete
+					Tree.pwd = [] #pwd
+					Tree.pwd2 = [] #pwd2
+					Tree.clear = [] #clear
+					Tree.empty = [] #empty
+	# delete empty folders and equal files
+	def delete(self, delete = 'both'):
+		if delete != 'dirs' and delete != 'files' and delete != 'both':
+			print('Arguments not found: the value of this arguments is "dirs" or "files" or "both"')
+			exit()
+		os.chdir(self.path)
+		pathing = os.popen('pwd').read().split('\n')[0]
+		ls = os.popen('ls').read().split('\n')
+		if len(ls) - 1 == 0:
+			Tree.empty.append(os.popen('pwd').read().split('\n')[0])
+		else:
+			for i in range(len(ls)):
+				if os.path.isdir(ls[i]) == True:
+					Tree.dirs.append(ls[i])																		# dirs
+					os.chdir(os.popen('cd "' + ls[i] + '"; pwd').read().split('\n')[0])
+					Tree.x = Tree.x + 1
+					Clean(os.popen('pwd').read().replace('\n', '')).delete(delete)
+					Tree.x = Tree.x - 1
+					os.chdir(pathing)
+				elif os.path.isfile('./' + ls[i]) == True:
+					if len(Tree.clear) != 0:
+						for x in range(len(Tree.clear)):
+							if md5(open(ls[i], 'rb').read()).hexdigest() == Tree.clear[x]:						# pwd2
+								Tree.pwd.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])	
+								Tree.delete.append(md5(open(ls[i], 'rb').read()).hexdigest())					# delete
+						if len(Tree.delete) != 0:
+							if md5(open(ls[i], 'rb').read()).hexdigest() != Tree.delete[len(Tree.delete) - 1]:
+								Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())					# clear
+						elif len(Tree.delete) == 0:
+							Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())						# clear
+					else:
+						Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())							# clear
+			if Tree.x == 0:
+				for i in range(len(Tree.empty)):
+					if delete == 'both' or delete == 'dirs':
+						os.system('rm -rf "' + Tree.empty[i] + '"')
+				for i in range(len(Tree.pwd)):
+					if delete == 'both' or delete == 'files':
+						os.system('rm -rf "' + Tree.pwd[i] + '"')
+				Tree.empty = []
+				Tree.pwd = []
+				Tree.dirs = []
+				Tree.delete = []
+				Tree.clear = []
