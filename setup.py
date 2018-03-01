df5f465c7c0ba14811c6fa984facf328c4154ab9
Matteo Monteleone <am.monteleone.am@gmail.com>
2018-03-01 13:36:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name = 'pythontree',
-	version = '1.3.3',
+	version = '1.4',
 	description = 'facilitate folder management',
 	license = "Apache-2.0",
 	author = 'MattVoid',
