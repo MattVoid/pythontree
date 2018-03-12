d19a341fa6fdc69ef42eeff004f60529fde91c56
Matteo Monteleone <am.monteleone.am@gmail.com>
2018-03-12 18:24:18 +0100

    Update __init__.py

--- pythontree/__init__.py
+++ pythontree/__init__.py
@@ -2,26 +2,27 @@ import os
 import sys
 from hashlib import md5
 class Tree:
+	clean = {'empty' : {'name' : [], 'path' : []}, 'duplicate' : {'original' : {'path' : [], 'name' : []}, 'name' : [], 'path' : []}}
+	roots = {'dir' : {'name' : [], 'path' : []}, 'file' : {'name' : [], 'path' : []}}
 	# Roots().roots() var
 	start = 0
 	end = 1
 	space = ''
 	# Clean().clean() and Clean.element() array
+	dirs = []
+	files = []
 	clear = []
 	delete = []
 	pwd = []
-	pwd2 = []
 	file = []
-	empty = []
 	# Roots().element() var and array
 	x = 0
-	dirs = []
-	files = []
 	# Roots().type()
 	type = []
 
 	def __init__(self, path = os.popen('cd; pwd').read().split('\n')[0]):
 		self.path = path
+
 class Roots(Tree):
 	def __init__(self, path):
 		super().__init__(path)
@@ -34,20 +35,21 @@ class Roots(Tree):
 		ls = os.popen('ls').read().split('\n')
 		for i in range(len(ls)):
 			if os.path.isdir(ls[i]) == True:
-				Tree.dirs.append(ls[i])
+				Tree.roots['dir']['path'].append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])
+				Tree.roots['dir']['name'].append(ls[i])
 				os.chdir(os.popen('cd "' + ls[i] + '"; pwd').read().split('\n')[0])
 				Roots(os.popen('pwd').read().replace('\n', '')).element()
 				os.chdir(pathing)
 			elif os.path.isfile('./' + ls[i]) == True:
-				Tree.files.append(ls[i])
+				Tree.roots['file']['path'].append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])
+				Tree.roots['file']['name'].append(ls[i])
 		Tree.x = Tree.x - 1
 		try:
-			return Tree.files, Tree.dirs
+			return Tree.roots
 		finally:
 			if Tree.x == 0:
 				# clear Roots().element() array
-				Tree.dirs = []
-				Tree.files = []
+				Tree.roots = {}
 	# graphic function, show the tree of the path
 	def type(self, type):
 		Tree.x = Tree.x + 1
@@ -126,12 +128,13 @@ class Clean(Tree):
 		pathing = os.popen('pwd').read().split('\n')[0]
 		ls = os.popen('ls').read().split('\n')
 		if len(ls) - 1 == 0:
-			Tree.empty.append(os.popen('pwd').read().split('\n')[0])											#empty
+			Tree.clean['empty']['path'].append(os.popen('pwd').read().split('\n')[0])
+			Tree.clean['empty']['name'].append(os.popen('pwd').read().split('\n')[0].split('/')[-1])
 		else:
 			for i in range(len(ls)):
 				if os.path.isdir(ls[i]) == True:
 					Tree.dirs.append(ls[i])
-					Tree.x = Tree.x + 1																			# dirs
+					Tree.x = Tree.x + 1
 					os.chdir(os.popen('cd "' + ls[i] + '"; pwd').read().split('\n')[0])
 					Clean(os.popen('pwd').read().replace('\n', '')).element()
 					Tree.x = Tree.x - 1
@@ -140,32 +143,32 @@ class Clean(Tree):
 					if len(Tree.clear) != 0:
 						for x in range(len(Tree.clear)):
 							if md5(open(ls[i], 'rb').read()).hexdigest() == Tree.clear[x]:
-								Tree.pwd2.append(Tree.files[x])													# pwd2
-								Tree.pwd.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])			# pwd
-								Tree.delete.append(md5(open(ls[i], 'rb').read()).hexdigest())					# delete
+								Tree.clean['duplicate']['path'].append(Tree.files[x])
+								Tree.clean['duplicate']['name'].append(Tree.files[x].split('/')[-1])
+								Tree.clean['duplicate']['original']['path'].append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])
+								Tree.clean['duplicate']['original']['name'].append(ls[i])
+								Tree.delete.append(md5(open(ls[i], 'rb').read()).hexdigest())                   # delete
 						if len(Tree.delete) != 0:
 							if md5(open(ls[i], 'rb').read()).hexdigest() != Tree.delete[len(Tree.delete) - 1]:
-								Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())					# clear
-								Tree.files.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])		# files
+								Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())                    # clear
+								Tree.files.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])       # files
 						elif len(Tree.delete) == 0:
-							Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())						# clear
-							Tree.files.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])			# files
+							Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())                        # clear
+							Tree.files.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])           # files
 					else:
-						Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())							# clear
-						Tree.files.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])				# files
+						Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())                            # clear
+						Tree.files.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])               # files
 			less = 1
 			try:
-				return Tree.empty, Tree.pwd, Tree.pwd2
+				return Tree.clean
 			finally:
 				if Tree.x == 0:
 					# clear Clean().element() array
 					Tree.dirs = [] #dirs
 					Tree.files = [] #files
 					Tree.delete = [] #delete
-					Tree.pwd = [] #pwd
-					Tree.pwd2 = [] #pwd2
 					Tree.clear = [] #clear
-					Tree.empty = [] #empty
+					Tree.clean = {}
 	# delete empty folders and equal files
 	def delete(self, delete = 'both'):
 		if delete != 'dirs' and delete != 'files' and delete != 'both':
@@ -179,7 +182,7 @@ class Clean(Tree):
 		else:
 			for i in range(len(ls)):
 				if os.path.isdir(ls[i]) == True:
-					Tree.dirs.append(ls[i])																		# dirs
+					Tree.dirs.append(ls[i])                                                                     # dirs
 					os.chdir(os.popen('cd "' + ls[i] + '"; pwd').read().split('\n')[0])
 					Tree.x = Tree.x + 1
 					Clean(os.popen('pwd').read().replace('\n', '')).delete(delete)
@@ -188,16 +191,16 @@ class Clean(Tree):
 				elif os.path.isfile('./' + ls[i]) == True:
 					if len(Tree.clear) != 0:
 						for x in range(len(Tree.clear)):
-							if md5(open(ls[i], 'rb').read()).hexdigest() == Tree.clear[x]:						# pwd2
-								Tree.pwd.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i])	
-								Tree.delete.append(md5(open(ls[i], 'rb').read()).hexdigest())					# delete
+							if md5(open(ls[i], 'rb').read()).hexdigest() == Tree.clear[x]:                      # pwd2
+								Tree.pwd.append(os.popen('pwd').read().replace('\n', '') + '/' + ls[i]) 
+								Tree.delete.append(md5(open(ls[i], 'rb').read()).hexdigest())                   # delete
 						if len(Tree.delete) != 0:
 							if md5(open(ls[i], 'rb').read()).hexdigest() != Tree.delete[len(Tree.delete) - 1]:
-								Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())					# clear
+								Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())                    # clear
 						elif len(Tree.delete) == 0:
-							Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())						# clear
+							Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())                        # clear
 					else:
-						Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())							# clear
+						Tree.clear.append(md5(open(ls[i], 'rb').read()).hexdigest())                            # clear
 			if Tree.x == 0:
 				for i in range(len(Tree.empty)):
 					if delete == 'both' or delete == 'dirs':
